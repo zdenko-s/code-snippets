1786b5f383d77f63659a20750098e95043c3d889
Zdenko Sadojevic <zdenko.sadojevic@sap.com>
2019-02-06 17:20:28 +0200

    Promise expanded into multi line function

--- node.js/set_timeout.js
+++ node.js/set_timeout.js
@@ -57,8 +57,12 @@ class ServiceBrokerCallback {
             console.log("awaiting Promise");
             yield new Promise(resolve => setTimeout(resolve, 2000));
             console.log("Promise 1 timeout elapsed.");
-            yield new Promise(resolve => setTimeout(resolve, 1000));
-            console.log("Promise 2 timeout elapsed.");
+            const ret = yield new Promise((resolve, reject) => {
+                setTimeout(() => {
+                    resolve(777);
+                }, 2000);
+            });
+            console.log(`Promise 2 timeout elapsed. Resolved value ${ret}`);
             this.dbRet = { conn: "ConnectionStringX" }; // Here comes real data
         });
     }
